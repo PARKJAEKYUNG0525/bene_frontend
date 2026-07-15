7d7f5659b5899467d4b4e86781f0c3703c6caad9
PARKJAEKYUNG <pjg0525@naver.com>
2026-07-15 11:16:38 +0900

    핸드폰 테두리 뺴기

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import PhoneFrame from './Components/PhoneFrame'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <PhoneFrame>
-      <App />
-    </PhoneFrame>
+    <App />
   </StrictMode>,
 )
